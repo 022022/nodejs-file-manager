0b140d90ad10ab5d894c51ef2bc1f6251a9e3a13
Zhenya <evgeegai@gmail.com>
2022-06-11 18:34:51 +0300

    feat: add cpus parameters

--- os.js
+++ os.js
@@ -7,9 +7,19 @@ export function os(arg){
   }
 
   if (arg === 'EOL') console.log(JSON.stringify(EOL));
-  if (arg === 'cpus') console.log(cpus().length);
   if (arg === 'homedir') console.log(homedir());
   if (arg === 'username') console.log(userInfo().username);
   if (arg === 'architecture') console.log(process.env['PROCESSOR_ARCHITECTURE']);
 
+  if (arg === 'cpus') cpusParams();
+
+}
+
+function cpusParams(){ // overall amount of CPUS plus model and clock rate (in GHz - speed/1000) for each of them
+  const cpusArr =  cpus();
+  console.log(`overall amount of CPUS: ${cpusArr.length}`);
+
+  cpusArr.forEach(item =>
+    console.log(`model: ${item.model} clock rate: ${item.speed/1000} GHz`)
+  )
 }
